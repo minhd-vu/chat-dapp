a47f654810b4554d3b232226226e2e092525e2ec
Minh Vu <minhd_vu@yahoo.com>
2021-12-13 04:44:26 -0500

    update natspec

--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -1,21 +1,31 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
+/**
+@title Chat dApp
+@author github.com/minhd-vu
+ */
 contract Chat {
     struct Message {
         address author; // author of the message
         string message; // chat message
     }
 
-    // A dynamically-sized array of `Message` structs.
+    // A dynamically-sized array of `Message` structs
     Message[] public messages;
 
-    // Send a message to the chat
+    /**
+    @notice Send a message to the chat
+    @param message The message being sent to the chat
+     */
     function send(string memory message) public {
         messages.push(Message(msg.sender, message));
     }
 
-    // Return all the messages sent
+    /**
+    @notice Get all the messages from the chat
+    @return Array of all the messages sent in chat
+    */
     function get() public view returns (Message[] memory) {
         return messages;
     }
