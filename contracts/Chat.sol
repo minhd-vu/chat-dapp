768d2ca3455ee1895ca92dcae4b54217fd35b1a6
Minh Vu <minhd_vu@yahoo.com>
2021-12-13 05:39:31 -0500

    update chat best practices

--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -18,7 +18,7 @@ contract Chat {
     @notice Send a message to the chat
     @param message The message being sent to the chat
      */
-    function send(string memory message) public {
+    function send(string calldata message) external {
         messages.push(Message(msg.sender, message));
     }
 
@@ -26,7 +26,21 @@ contract Chat {
     @notice Get all the messages from the chat
     @return Array of all the messages sent in chat
     */
-    function get() public view returns (Message[] memory) {
+    function get() external view returns (Message[] memory) {
         return messages;
+    }
+
+    /**
+    @notice Get the `n` latest messages from the chat
+    @param n The `n` latest messagess that were sent
+    @return Array of `n` messages sent in chat
+    @dev We cannot use slices because that is calldata only, and there is no negative indexing, so this is a little more involved than usual.
+    */
+    function get(uint256 n) external view returns (Message[] memory) {
+        Message[] memory m = new Message[](n);
+        for (uint256 i = 0; i < n; i++) {
+            m[i] = messages[messages.length - i - 1];
+        }
+        return m;
     }
 }
